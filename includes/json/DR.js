f87bfb12765024e5ab05446d37cf225e2dfd509a
Ioannis Toptsis <47627991+Ioannis-Toptsis@users.noreply.github.com>
2024-10-06 05:11:27 +0200

    v1.0

---
+++ includes/json/DR.js
@@ -0,0 +1,66 @@
+var dailies = {
+        "daily-quest-allowances": {
+        task: "Daily Quest",
+        url: "#",
+        short: true,
+        desc: "",
+        img: "contract",
+        boxcount: 10
+    },
+    "daily-amitoi": {
+        task: "Amitoi Expeditions",
+        url: "#",
+        short: true,
+        desc: "",
+        img: "placeholder",
+        boxcount: 1
+    },
+    "daily-dungeon-stamina": {
+        task: "Co-op Dungeon Stamina",
+        url: "#",
+        short: true,
+        desc: "",
+        img: "dimensional_points",
+        boxcount: 1
+    },
+    "daily-open-world-stamina": {
+        task: "Open World Dungeon Stamina",
+        url: "#",
+        short: true,
+        desc: "",
+        img: "abyssal_contract_tokens",
+        boxcount: 1
+    },
+    "daily-contracts": {
+        task: "Contracts",
+        url: "#",
+        short: true,
+        desc: "",
+        img: "placeholder",
+        boxcount: 1
+    },
+    "daily-vendor-items": {
+        task: "Vendor Item Purchases",
+        url: "#",
+        short: true,
+        desc: "",
+        img: "placeholder",
+        boxcount: 1
+    },
+    "daily-sundries-items": {
+        task: "Sundries Vendor Purchases",
+        url: "#",
+        short: true,
+        desc: "",
+        img: "placeholder",
+        boxcount: 1
+    },
+    "daily-battlepass": {
+        task: "Battlepass Missions",
+        url: "#",
+        short: true,
+        desc: "",
+        img: "placeholder",
+        boxcount: 1
+    }
+};
---
+++ includes/json/WR.js
@@ -0,0 +1,42 @@
+var weeklies = {
+    "weekly-missions": {
+        task: "Weekly Missions",
+        url: "#",
+        short: true,
+        desc: "",
+        img: "placeholder",
+        boxcount: 1
+    },
+    "weekly-gate-of-infinity": {
+        task: "Gate of Infinity",
+        url: "#",
+        short: true,
+        desc: "",
+        img: "placeholder",
+        boxcount: 1
+    },
+    "weekly-vendor-items": {
+        task: "Vendor Item Purchases",
+        url: "#",
+        short: true,
+        desc: "",
+        img: "placeholder",
+        boxcount: 1
+    },
+    "weekly-sundries-items": {
+        task: "Sundries Vendor Purchases",
+        url: "#",
+        short: true,
+        desc: "",
+        img: "placeholder",
+        boxcount: 1
+    },
+    "weekly-battlepass": {
+        task: "Battlepass Missions",
+        url: "#",
+        short: true,
+        desc: "",
+        img: "placeholder",
+        boxcount: 1
+    }
+};
